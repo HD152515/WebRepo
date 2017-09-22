35dbf049139f9f1e81179f15bacd99eae25d203e
박예원 <@DESKTOP-JUFKII7>
2017-09-22 14:28:07 +0900

    second commit

---
+++ WebClass/WebContent/jsp/menu.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="utf-8"%>
+<ul class="navbar-nav mr-auto">
+      <li class="nav-item" onmouseover = "menu_over(this);" onmouseout = "menu_out(this);">
+        <a class="nav-link" href="a.html">menu1</a>
+      </li>
+      <li class="nav-item" onmouseover = "menu_over(this);" onmouseout = "menu_out(this);">
+        <a class="nav-link" href="p.html">menu2</a>
+      </li>
+      <li class="nav-item" onmouseover = "menu_over(this);" onmouseout = "menu_out(this);">
+        <a class="nav-link" href="event.html">menu3</a>
+      </li>
+    </ul>
---
+++ WebClass/src/org/dimigo/servlet/HelloServlet.java
@@ -0,0 +1,80 @@
+package org.dimigo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HellosServlet
+ */
+@WebServlet(description = "1st Servlet", urlPatterns = { "/hello" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloServlet()
+    {
+    	
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// 입력데이터 처리
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		System.out.printf("id : %s  name : %s\n", id, name);
+		
+		// 출력 데이터 Content Type 설정
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Servlet Text </title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello, Servlet</h1>");
+		out.println("<h1>안녕, 서블릿</h1>");
+		out.println("<h2>id : " + id + ", name : " + name + "</h2>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		System.out.println("doPost 출력됨");
+		doGet(request, response);
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init()");
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("service()");
+		super.service(req, resp);
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("destroy()");
+	}
+}
